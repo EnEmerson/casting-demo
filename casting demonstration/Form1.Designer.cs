2ce3f6482636b79b924c895ae46018d4e05b3ca1
En Emerson <007emersone@gmail.com>
2018-05-24 23:43:16 -0500

    First commit

---
+++ casting demonstration/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace casting_demonstration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputbox = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputbox
+            // 
+            this.inputbox.Location = new System.Drawing.Point(435, 116);
+            this.inputbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.inputbox.Name = "inputbox";
+            this.inputbox.Size = new System.Drawing.Size(132, 22);
+            this.inputbox.TabIndex = 0;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel.Location = new System.Drawing.Point(249, 118);
+            this.inputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(150, 24);
+            this.inputLabel.TabIndex = 1;
+            this.inputLabel.Text = "Input a double:";
+            this.inputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(227, 382);
+            this.convertButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(100, 28);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "CONVERT";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(136, 214);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(276, 24);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Output double as an integer:";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // output
+            // 
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.output.Location = new System.Drawing.Point(437, 208);
+            this.output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(133, 28);
+            this.output.TabIndex = 4;
+            this.output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(523, 382);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 28);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "CLEAR";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 487);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.inputbox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputbox;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ casting demonstration/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace casting_demonstration
+{
+    public partial class Form1 : Form
+    {
+        //initializing variables
+        double input;
+        int outputVal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (inputbox.Text == "")                                        //checks to make sure something was actually entered into the box
+                {
+                    MessageBox.Show("enter some numbers into the textbox please");
+                    return;
+                }
+                else                                                            //converts the double to an integer by truncating 
+                {
+                    input = double.Parse(inputbox.Text);                        //the input is taken directly from the input box
+
+                    outputVal = (int)input;                                     //the output is designated by the input being "casted" to an integer from a double
+
+                    output.Text = outputVal.ToString();                         //the output is displayed as an integer in string format
+                }
+            }
+            catch                                                               //this makes sure no letters or any character other than a number    
+            {                                                                   //is entered so the program doesnt crash
+                MessageBox.Show("numbers only");
+                inputbox.Text = "";
+                input = 0;
+
+                return;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)              //resets everything to 0/empty
+        {
+            input = 0;
+
+            outputVal = 0;
+
+            inputbox.Text = "";
+
+            output.Text = "";
+        }
+
+    }
+}
